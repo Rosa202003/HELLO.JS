0f5951f36264d4ddb19934f2b0b8aef35139530c
Rosada Msuya <rosadamsuya2020@gmail.com>
2026-08-15 22:36:38 +0300

    this is ASSIGNMENT 1

--- intro.js
+++ intro.js
@@ -10,18 +10,10 @@ console.log('I am ', age, 'years old');
  // data types
  //string, number, boolean, null, undefined, object, array
  // array is a collection of data types
- const names=['Rosada', 'John', 'Doe'];
+ const names=['HTML', 'CSS', 'JAVASCRIPT'];
  console.log(names);
 
  // object is a collection of key value pairs
- const person={
-     name:'Rosada',
-     age:22,
-     isMarried:false,
-     hobbies:['coding', 'reading', 'swimming']
- }
- console.log(person);
-
 
  //functions
  function greet(name){
